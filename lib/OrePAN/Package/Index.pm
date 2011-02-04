ec85872d1e2e9247df6c155bb010651e099d1cdc
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-02-04 10:23:51 +0900

    write index file atomically.

--- lib/OrePAN/Package/Index.pm
+++ lib/OrePAN/Package/Index.pm
@@ -4,6 +4,9 @@ use warnings;
 use utf8;
 use Mouse;
 use IO::Zlib;
+use File::Temp qw(:mktemp);
+use File::Sync ();
+use Carp ();
 
 has filename => (
     is       => 'ro',
@@ -40,12 +43,21 @@ sub add {
 # TODO need flock?
 sub save {
     my ($self, ) = @_;
-    my $fh = IO::Zlib->new($self->filename, 'wb') or die $!;
+
+    # Because we do rename(2) atomically, temporary file must be in same
+    # partion with target file.
+    my $tmp = mktemp($self->filename . '.XXXXXX');
+
+    my $fh = IO::Zlib->new($tmp, 'wb') or die $!;
     print {$fh} "File:         02packages.details.txt\n\n";
     for my $key (sort keys %{$self->data}) {
         print {$fh} sprintf("%s\t%s\t%s\n", $key, $self->{data}->{$key}->[0] || 'undef', $self->{data}->{$key}->[1]);
     }
+    File::Sync::fsync($fh);
     close $fh;
+
+    rename( $tmp, $self->filename )
+      or Carp::croak("Cannot rename temporary file '$tmp' to @{[ $self->filename ]}: $!");
 }
 
 no Mouse; __PACKAGE__->meta->make_immutable;
