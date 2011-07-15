21a14facd6cb16ed668afd7fdcc9f3c6559ff377
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-07-15 12:07:56 +0900

    remove fucking dummy docs

--- lib/OrePAN.pm
+++ lib/OrePAN.pm
@@ -2,38 +2,5 @@ package OrePAN;
 use strict;
 use warnings;
 use 5.008001;
-our $VERSION = '0.01';
-
-
-
+# This is just a placeholder class.
 1;
-__END__
-
-=encoding utf8
-
-=head1 NAME
-
-OrePAN - Yet another DarkPAN manager
-
-=head1 SYNOPSIS
-
-  use OrePAN;
-
-=head1 DESCRIPTION
-
-OrePAN is
-
-=head1 AUTHOR
-
-Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
-
-=head1 SEE ALSO
-
-=head1 LICENSE
-
-Copyright (C) Tokuhiro Matsuno
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
