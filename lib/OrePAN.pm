8ec5bd37a509084e9a5960fa4e5559687127ad53
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-12-10 16:24:48 +0900

    Deprecated

--- lib/OrePAN.pm
+++ lib/OrePAN.pm
@@ -5,3 +5,10 @@ use 5.008001;
 # This is just a placeholder class.
 our $VERSION='0.08';
 1;
+__END__
+
+=head1 WARNINGS
+
+THIS MODULE WAS DEPRECATED. Use OrePAN2 instead.
+
+See L<https://github.com/tokuhirom/OrePAN2>.
